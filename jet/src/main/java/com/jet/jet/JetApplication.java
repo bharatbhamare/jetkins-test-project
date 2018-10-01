7be1e992fc5889f570d436d80a50ec9106010934
bharat.bhamare <bharat.bhamare@Dev56.glodom.local>
2018-10-01 11:13:23 +0530

    servlet init

--- jet/src/main/java/com/jet/jet/JetApplication.java
+++ jet/src/main/java/com/jet/jet/JetApplication.java
@@ -2,11 +2,30 @@ package com.jet.jet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jmx.export.MBeanExporter;
+
+
 
 @SpringBootApplication
-public class JetApplication {
+public class JetApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JetApplication.class, args);
 	}
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(JetApplication.class);
+	    }
+
+	  @Bean
+		 public MBeanExporter exporter()
+		 {
+		     final MBeanExporter exporter = new MBeanExporter();
+		     exporter.setAutodetect(true);
+		     exporter.setExcludedBeans("dataSource");
+		     return exporter;
+		 }
 }
